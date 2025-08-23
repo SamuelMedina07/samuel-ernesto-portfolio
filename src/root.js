97f38ed6c6ab263e952d26d6cc144ad65e67e88b
Samuel <samuelmedina2000@gmail.com>
2025-08-22 18:35:44 -0600

    Add rutas

---
+++ src/root.js
@@ -0,0 +1,16 @@
+export function ruta(){
+
+let base = "samuel-ernesto-portfolio"
+return base;
+
+}
+
+export function rute(){
+
+let base = "samuel-ernesto-portfolio/projects"
+return base;
+
+}
+
+
+
