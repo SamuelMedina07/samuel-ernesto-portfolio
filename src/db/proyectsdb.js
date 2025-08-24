fc760e7fae8838ec16ae4f94f98e873d62542cc3
SamuelMedina07 <semedina@uth.hn>
2025-08-24 12:27:44 -0600

    Update proyectsdb

--- src/db/proyectsdb.js
+++ src/db/proyectsdb.js
@@ -4,9 +4,9 @@ export default [
    title: "Sistema de Gestión Escolar y Colegio",
 description:
   "Proyecto desarrollado diseñado para la gestión integral de alumnos, calificaciones, asistencias, empleados, materias, consultas, usuarios y reportes. Este sistema demuestra mi experiencia en arquitectura de software, manejo de bases de datos relacionales y desarrollo de aplicaciones de escritorio escalables.",
-      github:"hola",
+      github:"",
     image: "Bosques_Pro.webp",
-    tags: [ TAGS.JAVA],
+    tags: [ TAGS.JAVA, TAGS.MYSQL, TAGS.JRXML],
   },
   {
     title: "Sistema de Gestión de Abarrotes",
@@ -14,6 +14,6 @@ description:
   "Aplicación de escritorio desarrollada en Java con NetBeans, conectada a MySQL y con reportes generados en iReport. Permite gestionar alumnos, calificaciones, asistencias, empleados, materias, grados y usuarios. Implementa el patrón MVC y genera reportes PDF profesionales. Proyecto académico con arquitectura modular y escalable.",
     link: "",
     image: "Abarrotes_Pro.webp",
-    tags: [TAGS.JAVA ],
+    tags: [TAGS.JAVA, TAGS.MYSQL, TAGS.JRXML],
   }
   ]
